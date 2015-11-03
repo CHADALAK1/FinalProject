542be0579b7bbbf25559f8c99fca4a64b62b5a66
Chad Reddick <chad_reddick2008@yahoo.com>
2015-11-03 13:57:49 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml
    	src/main.java
    fixed comment format
    
    Signed by CHADALAK1

--- src/main.java
+++ src/main.java
@@ -6,7 +6,7 @@ public class main
     public static void main(String[] args)
     {
         System.out.print("Hello");
-        System.out.print(" World");
+        System.out.print(" World ");
         System.out.println("AWESOME");
     }
 }
