f31865bd4b47a2ed3358103b587c41f37e7407ec
Chad Reddick <chad_reddick2008@yahoo.com>
2015-11-15 06:26:56 -0600

    Started to add base methods to Pawn and Controller class
    Also added a integer Health value to the Pawn Class
    
    Pawn:
      Move
      Interact
    
    Controller:
      Possess
    
    Signed by CHADALAK1

--- src/Controller.java
+++ src/Controller.java
@@ -3,5 +3,13 @@
  */
 public class Controller
 {
+    /*
+    Possess method
+    *Allows the Controller class to control and possess a Pawn
+    @param Vessel
+    *Vessel is for which pawn to possess
+    */
+    public void Possess(Pawn Vessel){}
+
 
 }
--- src/Pawn.java
+++ src/Pawn.java
@@ -3,5 +3,21 @@
  */
 public class Pawn
 {
+    /*
+    int Health
+    *Holds the Health value or the Pawn
+     */
+    public int Health;
 
+    /*
+    Move method
+    *allows the pawn to move in directions according to the Level grid
+     */
+    public void Move(){}
+
+    /*
+    Interact Method
+    *Allows the Pawn to interact with the world.
+    */
+    public void Interact(){}
 }
