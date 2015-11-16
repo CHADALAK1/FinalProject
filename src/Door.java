ff252af94cefa105d7b1e92760fb69c06d76e755
Chad Reddick <chad_reddick2008@yahoo.com>
2015-11-16 13:46:12 -0600

    Added Door and Key Classes
    Signed by CHADALAK1

---
+++ src/Door.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Chad Reddick on 11/16/15.
+ * <p>
+ *     Door class is an object inherited from Entity. This object can
+ *     either be already unlocked or require a Key to open
+ *     @see Key for locked details
+ * </p>
+ */
+public class Door extends Entity
+{
+
+}
---
+++ src/Key.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Chad Reddick on 11/16/15.
+ * <p>
+ *     Key class is an object inherited from Item. This
+ *     class is used to open barricades or doors needed to
+ *     open.
+ * </p>
+ */
+public class Key extends Item
+{
+
+}
