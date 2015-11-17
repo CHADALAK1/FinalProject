129df0ac4c2365d26d44f0cb25ba03285304c32c
LossyDragon <thedragon1012@gmail.com>
2015-11-16 21:24:53 -0600

    Started ground layout for the User Interface -Jake

--- src/main.java
+++ src/main.java
@@ -1,4 +1,7 @@
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.ImageIcon;
+import java.awt.Toolkit;
 
 /**
  * JAVA FINAL PROJECT
@@ -8,57 +11,111 @@ import javax.swing.*;
  */
 
 /**
- * Program: Test -- What up!!!
+ * Note 11/16; Added basic layout for the game, using Absolute layout though Oracle.
+ *
+ *
  */
 
-public class main
+public class main extends  JFrame
 {
-    private JLabel leMessage;
     private JPanel panel;
+
+    private JButton btnOne;
+    private JButton btnTwo;
+    private JTextArea StoryOutput;
+    private JTextField StoryInput;
+    private JSlider HitCrit;
+    private JList Inventory;
+    private JLabel Health;
+    private JLabel Armor;
 
     public main()
     {
-        final int Window_H = 100;
-        final int Window_W = 350;
+        final int Window_H = 768;
+        final int Window_W = 1024;
 
         //Create a test window.
-        JFrame window = new JFrame();
 
         //Title
-        window.setTitle("Hello World!");
+        setTitle("--Game Title--");
 
         //Set the size of the window, Width/Height
-        window.setSize(Window_W,Window_H);
+        setSize(Window_W,Window_H);
 
         //Center program on screen. OSX/Windows confirmed.
-        window.setLocationRelativeTo(null);
+        setLocationRelativeTo(null);
 
         //What happens when CLOSE is clicked.
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Disable Maximize button.
+        setResizable(false);
 
-        //Build and add panel().
-        panel();
-        window.add(panel);
-
-        //Display window.
-        window.setVisible(true);
-    }
-
-    private void panel(){
-
-        //JLabel message.
-        leMessage = new JLabel("WOW, SUCH JAVA");
-
-        //Create new JPanel.
+        //Construct a new JPanel.
         panel = new JPanel();
 
-        //Add leMessage to panel
-        panel.add(leMessage);
+        //PoC inventory...
+        //Is there an Inventory class?
+        String[] InvList = {"Sword", "Shield", "Mace"};
+
+        //Construct GUI components.
+        StoryInput = new JTextField (5);
+        StoryOutput = new JTextArea (5, 5);
+        HitCrit = new JSlider (0, 50);
+        Inventory = new JList (InvList);
+        Health = new JLabel ("Health: ##%");
+        Armor = new JLabel ("Armor ##%");
+        btnOne = new JButton ("Null");
+        btnTwo = new JButton ("Null");
+
+        //Setting component properties for Hit/Critical slider.
+        HitCrit.setOrientation (JSlider.HORIZONTAL);
+        HitCrit.setMinorTickSpacing (1);
+        HitCrit.setMajorTickSpacing (5);
+        HitCrit.setPaintTicks (true);
+        HitCrit.setPaintLabels (false);
+
+        //Set StoryOutput to Not Editable
+        StoryOutput.setEditable(false);
+
+        //Story line Example.
+        StoryOutput.setText("The quick brown fox jumps over the lazy dog.");
+
+        //Positioning of objects on panel. (Positioning is Rough guess as of 11/16)
+        //                      (xLeftRight, yUpdown, width, Height)
+        StoryOutput.setBounds    (10, 450, 995, 115);
+        StoryInput.setBounds   (10, 580, 995, 25);
+        Inventory.setBounds     (900, 650, 100, 75);
+        HitCrit.setBounds       (450, 680, 400, 50);
+        btnOne.setBounds        (10, 700, 100, 25);
+        btnTwo.setBounds        (120, 700, 100, 25);
+        Health.setBounds        (250, 700, 100, 25);
+        Armor.setBounds         (350, 700, 100, 25);
+
+        //Add components to panel.
+        add(btnOne);
+        add(btnTwo);
+        add(StoryOutput);
+        add(StoryInput);
+        add(HitCrit);
+        add(Inventory);
+        add(Health);
+        add(Armor);
+
+        /**
+         * add(panel) and setVisible(true) need to stay below this.
+         */
+
+        //add panel.
+        add(panel);
+        //Display window.
+        setVisible(true);
 
     }
 
-    //Main
+    //Start
     public static void main(String[] args){
-        new main();
+        main frame = new main();
     }
+
+
 }
