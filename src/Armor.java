ad0512ce94a17d55eceb84c8c99652e307dcf04a
Chad Reddick <chad_reddick2008@yahoo.com>
2015-12-06 13:30:58 -0600

    Added Armor class with Use
    Signed by CHADALAK1

---
+++ src/Armor.java
@@ -0,0 +1,20 @@
+/**
+ * Created by chadreddick on 12/6/15.
+ */
+public class Armor extends Item
+{
+    @Override
+    public void Use()
+    {
+        super.Use();
+        GetOwner().AddArmor(10);
+        for (int i = 0; i < GetOwner().GetInventory().size(); i++)
+        {
+            if (GetOwner().GetInventory().get(i) == this)
+            {
+                GetOwner().GetInventory().remove(i);
+            }
+        }
+        System.out.println("Added 10 Armor");
+    }
+}
